d2b77459183844c8b1d3c35cdc8dfc2125355abd
ayobami <ayoshmurda@gmail.com>
2025-06-25 20:44:55 +0000

    uno

--- index.js
+++ index.js
@@ -2,11 +2,18 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const {mongoClient, MongoClient} = require('mongodb');
+
+const client = new MongoClient(process.env.DB_URL)
+const db = client.db("urlshortner");
+const urls = db.collection("urls");
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
 app.use(cors());
+app.use(express.json())
+app.use(express.urlencoded({extended: true}));
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
@@ -15,7 +22,8 @@ app.get('/', function(req, res) {
 });
 
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
+app.post('/api/shorturl', function(req, res) {
+console.log(req.body())
   res.json({ greeting: 'hello API' });
 });
 
